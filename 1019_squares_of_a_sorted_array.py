225cb5ad7bb5bdaaed570a6a9e977db2aa1a023a
Maulik Darji <maulik.darji2005@gmail.com>
2026-03-02 22:34:20 +0530

    Update 1019_squares_of_a_sorted_array.py - LeetCode Problem 1019

--- 1019_squares_of_a_sorted_array.py
+++ 1019_squares_of_a_sorted_array.py
@@ -1,8 +1,44 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        sq_arr = []
-        for i in nums:
-            squ = i*i
-            sq_arr.append(squ)
-        sq_arr.sort()
-        return sq_arr
+        # sq_arr = []
+        # for i in nums:
+        #     squ = i*i
+        #     sq_arr.append(squ)
+        # sq_arr.sort()
+        # return sq_arr
+
+        # edge case
+        if not nums:
+            return nums
+        
+        if nums[0] >= 0:
+            return [num**2 for num in nums]
+            
+        if nums[-1] < 0:
+            return [num*num for num in reversed(nums)]
+
+
+        # finding first positive 
+        m = 0
+        for i,n in enumerate(nums):
+            if n>=0:
+                m = i
+                break
+        A, B = nums[m:], [-1*n for n in reversed(nums[:m])]
+        
+        def merge(A,B):
+            a = b = 0
+            ret = []
+            while a<len(A) and b<len(B):
+                if A[a] < B[b]:
+                    ret.append(A[a])
+                    a = a+1
+                else:
+                    ret.append(B[b])
+                    b = b+1
+            if a<len(A):
+                ret.extend(A[a:])
+            else:
+                ret.extend(B[b:])
+            return[n**2 for n in ret]
+        return(merge(A,B))
