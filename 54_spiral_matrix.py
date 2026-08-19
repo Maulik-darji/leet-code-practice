e5a4998c8822be5ce0c91e497be5feed18e7e015
Maulik Darji <maulik.darji2005@gmail.com>
2026-08-19 13:15:47 +0530

    Update 54_spiral_matrix.py - LeetCode Problem 54

--- 54_spiral_matrix.py
+++ 54_spiral_matrix.py
@@ -1,14 +1,43 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        res = []
-        while matrix:
-            res += (matrix.pop(0))
-            if matrix and matrix[0]:
-                for row in matrix:
-                    res.append(row.pop())
-            if matrix:
-                res += (matrix.pop()[::-1])
-            if matrix and matrix[0]:
-                for row in matrix[::-1]:
-                    res.append(row.pop(0))
-        return res
+
+        n = len(matrix)
+        m = len(matrix[0])
+
+        col_start, col_end = 0, m - 1
+        row_start, row_end = 0, n - 1
+
+        ans = []
+
+        while len(ans) < n * m:
+
+
+            for i in range(col_start, col_end + 1):
+                ans.append(matrix[row_start][i])
+            row_start += 1
+
+            if len(ans) == n * m:
+                break
+
+
+            for i in range(row_start, row_end + 1):
+                ans.append(matrix[i][col_end])
+            col_end -= 1
+
+            if len(ans) == n * m:
+                break
+
+
+            for i in range(col_end, col_start - 1, -1):
+                ans.append(matrix[row_end][i])
+            row_end -= 1
+
+            if len(ans) == n * m:
+                break
+
+
+            for i in range(row_end, row_start - 1, -1):
+                ans.append(matrix[i][col_start])
+            col_start += 1
+
+        return ans
